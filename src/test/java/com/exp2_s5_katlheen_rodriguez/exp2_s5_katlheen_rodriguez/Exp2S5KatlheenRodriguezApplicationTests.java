4501c514f4dae90809fa887c62bc8758dc205fb1
Katlheen Rodriguez <kv.rodriguezgarcia@gmail.com>
2025-04-08 21:24:13 -0400

    Primer Commit

---
+++ src/test/java/com/exp2_s5_katlheen_rodriguez/exp2_s5_katlheen_rodriguez/Exp2S5KatlheenRodriguezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exp2_s5_katlheen_rodriguez.exp2_s5_katlheen_rodriguez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp2S5KatlheenRodriguezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
