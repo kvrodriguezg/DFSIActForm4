0abcf1115472a8186441ad1e09cd706683b7db90
Katlheen Rodriguez <kv.rodriguezgarcia@gmail.com>
2025-04-08 22:09:27 -0400

    Repository

---
+++ src/main/java/com/exp2_s5_katlheen_rodriguez/exp2_s5_katlheen_rodriguez/repository/PeliculaRepository.java
@@ -0,0 +1,10 @@
+package com.exp2_s5_katlheen_rodriguez.exp2_s5_katlheen_rodriguez.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.exp2_s5_katlheen_rodriguez.exp2_s5_katlheen_rodriguez.model.Pelicula;
+
+@Repository
+public interface PeliculaRepository extends JpaRepository<Pelicula, Long>{
+    
+}
